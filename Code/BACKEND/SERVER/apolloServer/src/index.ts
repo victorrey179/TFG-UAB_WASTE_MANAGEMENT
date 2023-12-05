161d987278795c8e920095c0880b54d456eb30ad
victorrey179 <57214596+victorrey179@users.noreply.github.com>
2023-12-05 17:25:28 +0100

    gtaphql apollo server migration

---
+++ Code/BACKEND/SERVER/apolloServer/src/index.ts
@@ -0,0 +1,158 @@
+import { ApolloServer, gql } from "apollo-server";
+import bluecampusdb from "./bluecampusDB";
+import "./db";
+
+function getDurationInMilliseconds(duration: String): number {
+  const match = duration.match(/^(\d+)(min|h|d|sem|mes)$/);
+  if (!match) throw new Error("Invalid duration format");
+
+  const value = parseInt(match[1], 10);
+  const unit = match[2];
+  switch (unit) {
+    case "min":
+      return value * 60 * 1000;
+    case "h":
+      return value * 60 * 60 * 1000;
+    case "d":
+      return value * 24 * 60 * 60 * 1000;
+    case "sem":
+      return value * 7 * 24 * 60 * 60 * 1000;
+    case "mes":
+      return value * 30 * 24 * 60 * 60 * 1000; // Approximate month duration
+    default:
+      throw new Error("Invalid duration unit");
+  }
+}
+
+const typeDefs = gql`
+  type Measurement {
+    acceleration: [Int]!
+    distance: Int!
+    temperature: Int!
+    humidity: Int!
+    signal: Int!
+  }
+  type Record {
+    id: String!
+    date: String!
+    measurements: Measurement!
+  }
+  type Container {
+    idContainer: String!
+    data: [Record]
+  }
+  type Zone {
+    _id: String!
+    idZone: String!
+    containers: [Container]!
+  }
+  type DashboardData {
+    containerId: String!
+    records: [Record]!
+  }
+  type DashboardHtsData {
+    containerId: String!
+    date: String!
+    measurements: Measurement!
+  }
+  type Query {
+    numZones: Int!
+    allInfo: [Zone]!
+    zoneIds: [String]!
+    dashboardStatistics(zoneId: String!, duration: String!): [DashboardData]!
+    dashboardHTS(zoneId: String!): [DashboardHtsData]!
+  }
+`;
+interface DashboardArgs {
+  zoneId: string;
+  duration: string;
+}
+
+const resolvers = {
+  Query: {
+    numZones: async () => bluecampusdb.length,
+    allInfo: async () => bluecampusdb,
+    zoneIds: async () => bluecampusdb.map((zone) => zone.idZone),
+    dashboardStatistics: async (root: any, args: DashboardArgs) => {
+      const { zoneId, duration } = args;
+      const durationMs = getDurationInMilliseconds(duration);
+      const selectedZone = bluecampusdb.find((zone) => zone.idZone === zoneId);
+
+      if (!selectedZone) {
+        throw new Error("Zone not found");
+      }
+
+      const dashboardDataList = [];
+
+      for (const container of selectedZone.containers) {
+        let lastSelectedRecordTime = 0;
+        const records = [];
+
+        for (const record of container.data) {
+          const recordTime = new Date(record.date).getTime();
+
+          if (
+            lastSelectedRecordTime === 0 ||
+            recordTime - lastSelectedRecordTime >= durationMs
+          ) {
+            records.push(record);
+            lastSelectedRecordTime = recordTime;
+          }
+        }
+
+        if (records.length > 0) {
+          dashboardDataList.push({
+            containerId: container.idContainer,
+            records,
+          });
+        }
+      }
+
+      return dashboardDataList;
+    },
+    dashboardHTS: async (root: any, args: DashboardArgs) => {
+      const { zoneId } = args;
+      try {
+        // Encuentra la zona específica por su ID
+        const zoneDoc = bluecampusdb.find((zone) => zone.idZone === zoneId);
+
+        if (!zoneDoc) {
+          throw new Error("Zone not found");
+        }
+
+        const dashboardData = [];
+
+        for (const container of zoneDoc.containers) {
+          // Ordena los datos del contenedor por fecha y obtiene el registro más reciente
+          const sortedData = container.data.sort(
+            (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+          );
+
+          const lastRecord = sortedData[0];
+
+          if (lastRecord) {
+            dashboardData.push({
+              containerId: container.idContainer,
+              date: lastRecord.date,
+              measurements: lastRecord.measurements,
+            });
+          }
+        }
+
+        return dashboardData;
+      } catch (error) {
+        console.error("Error al obtener datos de la base de datos", error);
+        throw new Error("Server error");
+      }
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
---
+++ Code/BACKEND/SERVER/apolloServer/src/models/location.ts
@@ -0,0 +1,65 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+// Interfaces para TypeScript
+interface IMeasurement extends Document {
+  acceleration: number[];
+  distance: number;
+  temperature: number;
+  humidity: number;
+  signal: number;
+}
+
+interface IRecord extends Document {
+  id: string;
+  date: string;
+  measurements: IMeasurement;
+}
+
+interface IContainer extends Document {
+  idContainer: string;
+  data: IRecord[];
+}
+
+interface IZone extends Document {
+  _id: string;
+  idZone: string;
+  containers: IContainer[];
+}
+
+// Definición del esquema de Measurement
+const MeasurementSchema: Schema = new Schema({
+  acceleration: { type: [Number], required: true },
+  distance: { type: Number, required: true },
+  temperature: { type: Number, required: true },
+  humidity: { type: Number, required: true },
+  signal: { type: Number, required: true }
+});
+
+// Definición del esquema de Record
+const RecordSchema: Schema = new Schema({
+  id: { type: String, required: true },
+  date: { type: String, required: true },
+  measurements: { type: MeasurementSchema, required: true }
+});
+
+// Definición del esquema de Container
+const ContainerSchema: Schema = new Schema({
+  idContainer: { type: String, required: true },
+  data: [RecordSchema] // Un array de registros
+});
+
+// Definición del esquema de Zone
+const ZoneSchema: Schema = new Schema({
+  _id: { type: String, required: true },
+  idZone: { type: String, required: true },
+  containers: { type: [ContainerSchema], required: true }
+});
+
+// Creación de los modelos
+const MeasurementModel = mongoose.model<IMeasurement>('Measurement', MeasurementSchema);
+const RecordModel = mongoose.model<IRecord>('Record', RecordSchema);
+const ContainerModel = mongoose.model<IContainer>('Container', ContainerSchema);
+const ZoneModel = mongoose.model<IZone>('Zone', ZoneSchema);
+
+export { MeasurementModel, RecordModel, ContainerModel, ZoneModel };
+
